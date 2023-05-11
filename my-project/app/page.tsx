ba5c8f6977947a8f8c49c288eb0e55cf145079fb
HungTruong4689 <hungphuc4689@gmail.com>
2023-05-11 14:20:52 +0200

    change color module annotations

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -1,14 +1,16 @@
 import Image from 'next/image'
 
 export default function Home() {
+  console.log("test")
   return (
     <div>
-    <h1 className="bg-green-600 text-white">
-      Hello world!
-    </h1>
-    <h1 className="bg-green-600 text-white">
-      Hello world!
-    </h1>
+      <h1 className="bg-green-600 text-white">
+        Hello world!
+      </h1>
+      <h1 className="bg-green-600 text-white">
+        Hello world!
+      </h1>
+    
     </div>
   )
 }
