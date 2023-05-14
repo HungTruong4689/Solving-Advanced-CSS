9dc5ee815eb6d34ad4687c1c0139b390cabc518b
HungTruong4689 <hungphuc4689@gmail.com>
2023-05-14 22:01:07 +0200

    add vuejs color

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <h1 className="bg-green-600 text-white">
         Hello world!
       </h1>
-      <h1 className="bg-green-600 text-white">
+      <h1 className="bg-green-600 text-vuejs">
         Hello world!
       </h1>
     
