17a5ae4ae80313aaff85bb169344e4e10f7efd12
HungTruong4689 <hungphuc4689@gmail.com>
2023-05-17 22:22:20 +0200

    underline

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   console.log("test")
   return (
     <div>
-     <h1 className='text-3xl italic underline decoration-red-500 decoration-double'>Title 1</h1>
+     <h1 className='text-3xl '>Title 1</h1>
      <h1 className='text-2xl'>Title 2</h1>
      <h1 className='text-xl'>Title 3</h1>
-     <p className='text-base'>A regular paragraph</p>
+     <p className='text-base underline decoration-red-500 decoration-wavy decoration-1 underline-offset-1'>A regular paragraph</p>
      <p className='test-sm'>A description paragraph</p>
      <p className='test-xs note'>A little node</p>
     
