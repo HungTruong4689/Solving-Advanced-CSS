0447c2731fc82ed0cae539e314ef445e31f8ee24
HungTruong4689 <hungphuc4689@gmail.com>
2023-05-17 22:24:15 +0200

    line-height

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
      <h1 className='text-3xl '>Title 1</h1>
      <h1 className='text-2xl'>Title 2</h1>
      <h1 className='text-xl'>Title 3</h1>
-     <p className='text-base underline decoration-red-500 decoration-wavy decoration-1 underline-offset-1'>A regular paragraph</p>
+     <p className='text-base underline decoration-red-500 decoration-wavy decoration-1 underline-offset-1 leading-normal'>A regular paragraph</p>
      <p className='test-sm'>A description paragraph</p>
      <p className='test-xs note'>A little node</p>
     
