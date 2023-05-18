e812184d13428d29e970e2fe372e3bcb6051c719
HungTruong4689 <hungphuc4689@gmail.com>
2023-05-18 22:03:03 +0200

    flex

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -3,10 +3,11 @@ import Image from 'next/image'
 export default function Home() {
   console.log("test")
   return (
-    <div className='flex w-full'>
-      <div className="w-1/5 h-16 bg-red-500" >Sidebar
+    <div className='flex Parent'>
+      <div>Header</div>
+      <div className="" >Sidebar
       </div>
-      <div className="w-4/5 h-16 bg-green-500" >Main content
+      <div className="" >Main content
       </div>
     </div>
   )
