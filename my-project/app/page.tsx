c28b8bd0efc9e8ce3ef5e5aee9281d22e23ace70
HungTruong4689 <hungphuc4689@gmail.com>
2023-05-18 21:52:06 +0200

    width fraction

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -3,16 +3,11 @@ import Image from 'next/image'
 export default function Home() {
   console.log("test")
   return (
-    <div>
-     {/* <h1 className='text-3xl '>Title 1</h1>
-     <h1 className='text-2xl'>Title 2</h1>
-     <h1 className='text-xl'>Title 3</h1>
-     <p className='text-base underline decoration-red-500 decoration-wavy decoration-1 underline-offset-1 leading-normal'>A regular paragraph</p>
-     <p className='test-sm uppercase'>A description paragraph</p>
-     <p className='test-xs note lowercase'>A little node</p> */}
-     <p className=' m-4 font-bold text-white bg-blue-500'>Box with margin</p>
-     <p className=' -mt-8 font-bold text-white bg-red-500'>Box with padding</p>
-    
+    <div className='flex w-full'>
+      <div className="w-1/5 h-16 bg-red-500" >Sidebar
+      </div>
+      <div className="w-4/5 h-16 bg-green-500" >Main content
+      </div>
     </div>
   )
 }
