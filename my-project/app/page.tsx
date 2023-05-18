c20cb813b70104d774fa4eac7889560eb6eeac46
HungTruong4689 <hungphuc4689@gmail.com>
2023-05-18 22:22:08 +0200

    flex-col && flex-row

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -3,11 +3,14 @@ import Image from 'next/image'
 export default function Home() {
   console.log("test")
   return (
-    <div className='flex Parent'>
+    <div className='Parent'>
       <div>Header</div>
-      <div className="" >Sidebar
+      <div className="flex flex-row" >
+      <div>Main content</div>
       </div>
-      <div className="" >Main content
+      <div className="flex flex-col" >
+        <div>Sidebar</div>
+        <div>Menu</div>
       </div>
     </div>
   )
