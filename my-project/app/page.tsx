73778e7fb417b3954e4723f5cb8706b8ba72df87
HungTruong4689 <hungphuc4689@gmail.com>
2023-05-19 22:51:19 +0200

    wrap flex

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -6,12 +6,12 @@ export default function Home() {
     <div className='Parent'>
       <div>Header</div>
       <div className="flex flex-col md:flex-row" >
-        <div className="flex w-full flex-nowrap">
-          <div className='w-full grow'>01</div>
-          <div className='w-full grow'>02</div>
-          <div className='w-full grow'>03</div>
-          <div className='w-full grow'>04</div>
-          <div className='w-full grow'>05</div>
+        <div className="flex w-full flex-wrap">
+          <div className='w-32 grow'>01</div>
+          <div className='w-32 grow'>02</div>
+          <div className='w-32 grow'>03</div>
+          <div className='w-32 grow'>04</div>
+          <div className='w-32 grow'>05</div>
         </div>
         
       </div>
