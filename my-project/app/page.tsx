13365f7f4351f3c017b122943535c95b4ce21b31
HungTruong4689 <hungphuc4689@gmail.com>
2023-05-20 23:42:57 +0200

    col-span

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -5,11 +5,13 @@ export default function Home() {
   return (
     <div className='h-screen text-white Parent bg-slate-900'>
       <div className="container mx-auto">
-        <div className="grid grid-cols-4 gap-1">
+        <div className="grid grid-cols-3 gap-4">
           <div className="bg-sky-500 p-6 rounded-lg">First element</div>
           <div className="bg-sky-500 p-6 rounded-lg">Second element</div>
           <div className="bg-sky-500 p-6 rounded-lg">Third element</div>
-          <div className="bg-sky-500 p-6 rounded-lg">Forth element</div>
+          <div className="bg-sky-500 p-6 rounded-lg col-span-2">Fourth element</div>
+          <div className="bg-sky-500 p-6 rounded-lg">Fifth element</div>
+          <div className="bg-sky-500 p-6 rounded-lg">Sixth element</div>
         </div>
       </div>
         
