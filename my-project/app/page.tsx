361c9a61b1af67ef8d78df241813aa92b36d18dd
HungTruong4689 <hungphuc4689@gmail.com>
2023-05-21 20:50:16 +0200

    float

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -6,6 +6,10 @@ export default function Home() {
         <div className="h-screen text-white Parent bg-slate-900">
             <div className="container mx-auto columns-lg bg-red-500">
                 <h1>Hello</h1>
+                <img
+                    src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*"
+                    className="float-left w-64"
+                />
             </div>
         </div>
     )
