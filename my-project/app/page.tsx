0b3b3d9d3d0f22ba03d6fffd42e88a3956b9d808
HungTruong4689 <hungphuc4689@gmail.com>
2023-05-21 19:59:38 +0200

    Prettier fix

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -1,21 +1,10 @@
 import Image from 'next/image'
 
 export default function Home() {
-  console.log("test")
-  return (
-    <div className='h-screen text-white Parent bg-slate-900'>
-      <div className="container mx-auto">
-        <div className="grid grid-rows-6 gap-4">
-          <div className="row-span-4 bg-sky-500 p-6 rounded-lg">First element</div>
-          <div className="bg-sky-500 p-6 rounded-lg">Second element</div>
-          <div className="col-span-2 bg-sky-500 p-6 rounded-lg">Third element</div>
-          <div className="bg-sky-500 p-6 rounded-lg col-span-2">Fourth element</div>
-          
+    console.log('test')
+    return (
+        <div className="h-screen text-white Parent bg-slate-900">
+            <div className="container"></div>
         </div>
-      </div>
-        
-     
-      
-    </div>
-  )
+    )
 }
