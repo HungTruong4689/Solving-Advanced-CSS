a00205a28bfb525e0d862c1db7349338bd07c9cc
HungTruong4689 <hungphuc4689@gmail.com>
2023-05-21 21:03:43 +0200

    top pos

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     console.log('test')
     return (
         <div className="h-screen text-white Parent bg-slate-900">
-            <div className="container mx-auto columns-lg bg-red-500">
+            <div className="container mx-auto columns-lg top-5 bg-red-500">
                 <h1 className="relative">Hello</h1>
                 <img
                     src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*"
