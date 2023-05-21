2727cbd3d68d550ccb22fe79db8e83bb41558e41
HungTruong4689 <hungphuc4689@gmail.com>
2023-05-21 20:23:33 +0200

    columns lg

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     console.log('test')
     return (
         <div className="h-screen text-white Parent bg-slate-900">
-            <div className="container mx-auto bg-red-500">
+            <div className="container mx-auto columns-lg bg-red-500">
                 <h1>Hello</h1>
             </div>
         </div>
