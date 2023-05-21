0e2af894d8bcfbdac47270fc9b11238c2c4c9177
HungTruong4689 <hungphuc4689@gmail.com>
2023-05-21 20:15:16 +0200

    container

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
     console.log('test')
     return (
         <div className="h-screen text-white Parent bg-slate-900">
-            <div className="container"></div>
+            <div className="container bg-red-500">
+                <h1>Hello</h1>
+            </div>
         </div>
     )
 }
