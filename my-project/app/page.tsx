e03b013c06f91ddc62bec4c24ebfb9fce0b00f9f
HungTruong4689 <hungphuc4689@gmail.com>
2023-05-22 21:47:49 +0200

    border color

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     console.log('test')
     return (
         <div className="h-screen text-white Parent bg-slate-900">
-            <div className="container divide-y columns-lg top-5 bg-red-500">
+            <div className="container divide-y border-red-300 columns-lg top-5 bg-red-500">
                 <div className="">01</div>
                 <div className="">01</div>
                 <div className="">01</div>
