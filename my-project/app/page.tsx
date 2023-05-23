b9c3b4b46c33c70fc8bc4be174779e8e7cee9749
HungTruong4689 <hungphuc4689@gmail.com>
2023-05-23 22:31:26 +0200

    transition

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
                 />
                 <h1 className="text-2xl">Hello</h1>
                 <p className="mb-4">This is my div</p>
-                <button className="px-3 py-2 rounded-lg cursor-pointer bg-cyan-500 shadow-md shadow-cyan-500/50 hover:-translate-y-1">
+                <button className="px-3 py-2 rounded-lg cursor-pointer bg-cyan-500 shadow-md shadow-cyan-500/50 hover:-translate-y-1 transition">
                     Say Hello
                 </button>
             </div>
