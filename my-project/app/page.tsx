07b63b23ebcabd4df8fe4b181e8924134692bac9
HungTruong4689 <hungphuc4689@gmail.com>
2023-05-23 22:11:35 +0200

    color opacity

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
     console.log('test')
     return (
         <div className="h-screen p-4 Parent">
-            <div className="inline-block p-4 text-black bg-white border rounded-lg shadow-md shadow-cyan-500">
+            <div className="inline-block p-4 text-black bg-white border rounded-lg ">
                 <h1 className="text-2xl">Hello</h1>
                 <p className="mb-4">This is my div</p>
-                <button className="px-3 py-2 rounded-lg cursor-pointer bg-cyan-500">
+                <button className="px-3 py-2 rounded-lg cursor-pointer bg-cyan-500 shadow-md shadow-cyan-500/50">
                     Say Hello
                 </button>
             </div>
