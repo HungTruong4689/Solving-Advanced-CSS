faa553bc5eff2fe5ce7015c30965b34b70fa00a3
HungTruong4689 <hungphuc4689@gmail.com>
2023-05-24 22:40:04 +0200

    html function

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -4,16 +4,25 @@ export default function Home() {
     console.log('test')
     return (
         <div className="h-screen p-4 Parent">
-            <div className="inline-block p-4 text-black bg-white border rounded-lg ">
-                <img
-                    src="https://wallpaperaccess.com/full/1540354.jpg"
-                    className="w-64 mb-4 invert -skew-y-12"
-                />
-                <h1 className="text-2xl">Hello</h1>
-                <p className="mb-4">This is my div</p>
-                <button className="px-3 py-2 rounded-lg cursor-pointer bg-cyan-500 shadow-md shadow-cyan-500/50 hover:-translate-y-1 duration-100 transition animate-spin">
-                    Say Hello
-                </button>
+            <h1>This is the title</h1>
+            <h2>This is subtitle</h2>
+            <p>This is paragraph</p>
+            <a href="">This is an anchor</a>
+            <div className="">
+                <button>This is a button</button>
+            </div>
+            <div className="">
+                <input type="text" />
+            </div>
+            <div className="">
+                <input type="checkbox" className="checkbox" />
+                <label htmlFor="checkbox"></label>
+            </div>
+            <div className="">
+                <select name="" id="">
+                    <option value="">Option 1</option>
+                    <option value="">Option 2</option>
+                </select>
             </div>
         </div>
     )
