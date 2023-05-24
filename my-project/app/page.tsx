f06d6b39308796174a481909607ca5ce96e8bf9e
HungTruong4689 <hungphuc4689@gmail.com>
2023-05-24 22:34:21 +0200

    skew-y

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
             <div className="inline-block p-4 text-black bg-white border rounded-lg ">
                 <img
                     src="https://wallpaperaccess.com/full/1540354.jpg"
-                    className="w-64 mb-4 invert"
+                    className="w-64 mb-4 invert -skew-y-12"
                 />
                 <h1 className="text-2xl">Hello</h1>
                 <p className="mb-4">This is my div</p>
