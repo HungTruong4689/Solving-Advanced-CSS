29da2e077fe6aa9b4dc645c4898452a158486301
HungTruong4689 <hungphuc4689@gmail.com>
2023-05-25 23:17:16 +0200

    table

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -24,9 +24,35 @@ export default function Home() {
                     <option value="">Option 2</option>
                 </select>
             </div>
-            <div className="">
-                <div className="contrast-125 hover:contrast-150">Constrast</div>
-            </div>
+
+            <table className="border-collapse border border-slate-500 ...">
+                <thead>
+                    <tr>
+                        <th className="border border-slate-600 ...">State</th>
+                        <th className="border border-slate-600 ...">City</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="border border-slate-700 ...">Indiana</td>
+                        <td className="border border-slate-700 ...">
+                            Indianapolis
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="border border-slate-700 ...">Ohio</td>
+                        <td className="border border-slate-700 ...">
+                            Columbus
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="border border-slate-700 ...">
+                            Michigan
+                        </td>
+                        <td className="border border-slate-700 ...">Detroit</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
