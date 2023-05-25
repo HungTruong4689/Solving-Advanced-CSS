bcfa5914051ab445c57b0eefbe34e31994cbe33c
HungTruong4689 <hungphuc4689@gmail.com>
2023-05-25 23:14:36 +0200

    blur test

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <div className="h-screen p-4 Parent">
             <h1 className="">This is the title</h1>
-            <h2 className="text-xl font-semibold">This is subtitle</h2>
+            <h2 className="text-xl ">This is subtitle</h2>
             <p className="text-base">This is paragraph</p>
             <a href="">This is an anchor</a>
             <div className="">
@@ -23,6 +23,12 @@ export default function Home() {
                     <option value="">Option 1</option>
                     <option value="">Option 2</option>
                 </select>
+            </div>
+            <div className="">
+                <div className="blur-none ..."></div>
+                <div className="blur-sm ..."></div>
+                <div className="blur-lg ..."></div>
+                <div className="blur-2xl ..."></div>
             </div>
         </div>
     )
