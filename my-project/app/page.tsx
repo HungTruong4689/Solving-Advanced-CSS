f9052d12bece36e3b66d77035a14fdc019998d31
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-06 23:07:04 +0200

    absolute

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -170,6 +170,21 @@ export default function Home() {
                 <img className="object-cover h-48 w-96 ..." />
             </div>
             <div className="static ...">
+                <div className="static ...">
+                    <p>Static child</p>
+                </div>
+                <div className="inline-block ...">
+                    <p>Static sibling</p>
+                </div>
+
+                <div className="absolute ...">
+                    <p>Absolute child</p>
+                </div>
+                <div className="inline-block ...">
+                    <p>Static sibling</p>
+                </div>
+            </div>
+            <div className="static ...">
                 <p>Static parent</p>
                 <div className="absolute bottom-0 left-0 ...">
                     <p>Absolute child</p>
