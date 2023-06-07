51e124797e0687dc9f66cce7de393306a6501b64
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-07 22:45:28 +0200

    flex no-wrap

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -199,6 +199,11 @@ export default function Home() {
                 <div>02</div>
                 <div>03</div>
             </div>
+            <div className="flex flex-nowrap">
+                <div>01</div>
+                <div>02</div>
+                <div>03</div>
+            </div>
             <div className="flex flex-row-reverse ...">
                 <div>01</div>
                 <div>02</div>
