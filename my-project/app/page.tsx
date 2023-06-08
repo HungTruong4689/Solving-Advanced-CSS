89ea3c2ae82de5e04f46a55f8d73c0f8effb6205
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-08 22:25:09 +0200

    flex grow

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -214,6 +214,11 @@ export default function Home() {
                 <div>02</div>
                 <div>03</div>
             </div>
+            <div className="flex ...">
+                <div className="flex-none w-14 h-14 ...">01</div>
+                <div className="grow h-14 ...">02</div>
+                <div className="flex-none w-14 h-14 ...">03</div>
+            </div>
             <div className="flex flex-wrap-reverse">
                 <div>01</div>
                 <div>02</div>
