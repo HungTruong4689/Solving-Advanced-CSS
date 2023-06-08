c3e104974cdc13f990dcfd3c009d9117d35d98f2
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-08 22:45:00 +0200

    gap grid

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -219,6 +219,12 @@ export default function Home() {
 
                 <div>09</div>
             </div>
+            <div className="grid gap-4 grid-cols-2">
+                <div>01</div>
+                <div>02</div>
+                <div>03</div>
+                <div>04</div>
+            </div>
             <div className="flex justify-between ...">
                 <div className="order-last">01</div>
                 <div>02</div>
