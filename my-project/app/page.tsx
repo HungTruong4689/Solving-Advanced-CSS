d5a2b9f85599aa6a8a7177fdd968e5d6f3079376
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-09 20:16:31 +0200

    grid-flow-row-dense

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -226,6 +226,11 @@ export default function Home() {
                 <div>04</div>
                 <div>05</div>
             </div>
+            <div className="grid grid-flow-col auto-cols-max">
+                <div>01</div>
+                <div>02</div>
+                <div>03</div>
+            </div>
             <div className="grid gap-4 grid-cols-2">
                 <div>01</div>
                 <div>02</div>
