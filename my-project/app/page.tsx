39b700dce6bfbcf9f71fa4dea9c7db5f2764e27b
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-10 23:08:59 +0200

    content around

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -266,6 +266,13 @@ export default function Home() {
                 <div>02</div>
                 <div>03</div>
             </div>
+            <div className="h-56 grid grid-cols-3 gap-4 content-around ...">
+                <div>01</div>
+                <div>02</div>
+                <div>03</div>
+                <div>04</div>
+                <div>05</div>
+            </div>
             <div className="grid grid-cols-2 gap-4 place-content-center h-48 ...">
                 <div>01</div>
                 <div>02</div>
