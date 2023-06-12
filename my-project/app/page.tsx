083c7cd1102b099c58b46b66f6fd9414918847b2
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-12 09:12:22 +0200

    place-content-end

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -424,6 +424,12 @@ export default function Home() {
                 <div>03</div>
                 <div>04</div>
             </div>
+            <div className="grid grid-cols-2 gap-4 place-content-end h-48 ...">
+                <div>01</div>
+                <div>02</div>
+                <div>03</div>
+                <div>04</div>
+            </div>
             <div className="flex flex-wrap-reverse">
                 <div>01</div>
                 <div>02</div>
