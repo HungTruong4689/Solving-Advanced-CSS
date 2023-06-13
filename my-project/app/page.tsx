a5ef7ad6f3e0a2e65533889b99f1e07a98e35d63
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-13 11:52:34 +0200

    bottom left corner

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -594,6 +594,9 @@ export default function Home() {
             <div className="relative h-32 w-32 ...">
                 <div className="absolute inset-y-0 right-0 w-16 ...">06</div>
             </div>
+            <div className="relative h-32 w-32 ...">
+                <div className="absolute bottom-0 left-0 h-16 w-16 ...">07</div>
+            </div>
             <div className="relative">
                 <div className="fixed top-0 left-0 right-0">Contacts</div>
                 <div>
