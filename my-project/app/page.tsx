4134f558eb8713dbcad0453430e2087106a90c6b
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-14 21:08:14 +0200

    font-sans

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -508,6 +508,7 @@ export default function Home() {
             <p className="text-justify ...">
                 So I started to walk into the water...
             </p>
+            <p className="font-sans ...">The quick brown fox ...</p>
             <div className="grid grid-cols-3 gap-4 ...">
                 <div>01</div>
                 <div className="place-self-auto ...">02</div>
