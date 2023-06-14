8cc195752e943cbaef8ea5d86f01e71626fa2e42
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-14 21:11:35 +0200

    text-lg

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -513,6 +513,7 @@ export default function Home() {
             <p className="font-sans ...">The quick brown fox ...</p>
             <p className="font-serif ...">The quick brown fox ...</p>
             <p className="text-base ...">The quick brown fox ...</p>
+            <p className="text-lg ...">The quick brown fox ...</p>
             <div className="grid grid-cols-3 gap-4 ...">
                 <div>01</div>
                 <div className="place-self-auto ...">02</div>
