022000d9a3f9c01ab287cb544b933e3954ef04b3
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-16 10:38:39 +0200

    stacked-fractions

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -543,6 +543,8 @@ export default function Home() {
             <p className="proportional-nums ...">12121</p>
             <p className="tabular-nums ...">12121</p>
             <p className="diagonal-fractions ...">1/2 3/4 5/6</p>
+            <p className="stacked-fractions ...">1/2 3/4 5/6</p>
+
             <div className="grid grid-cols-3 gap-4 ...">
                 <div>01</div>
                 <div className="place-self-auto ...">02</div>
