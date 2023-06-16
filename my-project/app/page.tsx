97e3d8206fbe15d24ab6046451dcb6b1090a7895
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-16 10:23:20 +0200

    variant number

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -534,6 +534,8 @@ export default function Home() {
             <p className="font-normal ...">The quick brown fox ...</p>
             <p className="font-medium ...">The quick brown fox ...</p>
             <p className="font-semibold ...">The quick brown fox ...</p>
+            <p className="font-bold ...">The quick brown fox ...</p>
+            <p className="ordinal slashed-zero tabular-nums ...">1234567890</p>
             <div className="grid grid-cols-3 gap-4 ...">
                 <div>01</div>
                 <div className="place-self-auto ...">02</div>
