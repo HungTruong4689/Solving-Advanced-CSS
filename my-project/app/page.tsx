c4809f3a6c05b391794499988b4c7b7cae82307e
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-16 10:27:04 +0200

    lining-nums

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -538,6 +538,7 @@ export default function Home() {
             <p className="ordinal slashed-zero tabular-nums ...">1234567890</p>
             <p className="ordinal ...">1st</p>
             <p className="slashed-zero ...">0</p>
+            <p className="lining-nums ...">1234567890</p>
             <div className="grid grid-cols-3 gap-4 ...">
                 <div>01</div>
                 <div className="place-self-auto ...">02</div>
