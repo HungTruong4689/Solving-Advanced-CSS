5ecdb47adc9870958986d9e3c51adebdbcc00419
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-18 22:58:12 +0200

    list-outside

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -673,6 +673,9 @@ export default function Home() {
                     </div>
                 </div>
             </div>
+            <ul className="list-outside ...">
+                <li>5 cups chopped Porcini mushrooms</li>
+            </ul>
             <div className="relative h-32 w-32 ...">
                 <div className="absolute left-0 top-0 h-16 w-16 ...">01</div>
             </div>
