57908ce1c9bd17d28f456306ef4139013d1ae13a
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-19 16:03:34 +0200

    decoration-solid

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -739,6 +739,9 @@ export default function Home() {
                     fights.
                 </p>
             </div>
+            <p className="underline decoration-solid ...">
+                The quick brown fox...
+            </p>
             <div className="relative h-32 w-32 ...">
                 <div className="absolute inset-y-0 left-0 w-16 ...">04</div>
             </div>
