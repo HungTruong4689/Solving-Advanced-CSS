befb401f645cebd1c68065ccc1c6d4b6d3b86412
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-19 15:45:19 +0200

    text-justify

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -712,6 +712,9 @@ export default function Home() {
             <p className="text-right ...">
                 So I started to walk into the water...
             </p>
+            <p className="text-justify ...">
+                So I started to walk into the water...
+            </p>
             <div className="relative h-32 w-32 ...">
                 <div className="absolute inset-y-0 left-0 w-16 ...">04</div>
             </div>
