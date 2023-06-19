eb58b6a11c95bfd18beb9feef594d2a15416cc94
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-19 15:44:49 +0200

    text-right

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -709,6 +709,9 @@ export default function Home() {
             <p className="text-center ...">
                 So I started to walk into the water...
             </p>
+            <p className="text-right ...">
+                So I started to walk into the water...
+            </p>
             <div className="relative h-32 w-32 ...">
                 <div className="absolute inset-y-0 left-0 w-16 ...">04</div>
             </div>
