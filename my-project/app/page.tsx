cd996258670d9df32d3a6c31b1a72fd4a47e28f3
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-20 22:45:04 +0200

    uppercase

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -781,6 +781,7 @@ export default function Home() {
             <div className="relative h-32 w-32 ...">
                 <div className="absolute bottom-0 left-0 h-16 w-16 ...">07</div>
             </div>
+            <p className="uppercase ...">The quick brown fox ...</p>
             <p className="normal-case ...">The quick brown fox ...</p>
             <div className="relative">
                 <div className="fixed top-0 left-0 right-0">Contacts</div>
