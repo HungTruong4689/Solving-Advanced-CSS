ccd67d41e8c54a44d986b9c547b876fff44828ea
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-20 22:42:32 +0200

    underline-offset-2

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -760,6 +760,9 @@ export default function Home() {
             <p className="underline underline-offset-1 ...">
                 The quick brown fox...
             </p>
+            <p className="underline underline-offset-2 ...">
+                The quick brown fox...
+            </p>
             <div className="relative h-32 w-32 ...">
                 <div className="absolute inset-y-0 left-0 w-16 ...">04</div>
             </div>
