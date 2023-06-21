321ab9257b8411c3058a09b912f1a5b1eddbeaa5
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-21 22:45:26 +0200

    margin right

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -792,6 +792,7 @@ export default function Home() {
             <div className="ps-8 ...">ps-8</div>
             <div className="pe-8 ...">pe-8</div>
             <div className="mt-6 ...">mt-6</div>
+            <div className="mr-4 ...">mr-4</div>
 
             <p className="uppercase ...">The quick brown fox ...</p>
             <p className="normal-case ...">The quick brown fox ...</p>
