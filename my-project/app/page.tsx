961b15e3fd82e35c0f79924ad6fbef6a36807df3
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-25 21:17:48 +0200

    line-clamp-3

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -684,6 +684,24 @@ export default function Home() {
             </div>
             <p className="tracking-normal ...">The quick brown fox ...</p>
             <p className="tracking-wide ...">The quick brown fox ...</p>
+            <article>
+                <time>Mar 10, 2020</time>
+                <h2>Boost your conversion rate</h2>
+                <p className="line-clamp-3">
+                    Nulla dolor velit adipisicing duis excepteur esse in duis
+                    nostrud occaecat mollit incididunt deserunt sunt. Ut ut sunt
+                    laborum ex occaecat eu tempor labore enim adipisicing minim
+                    ad. Est in quis eu dolore occaecat excepteur fugiat dolore
+                    nisi aliqua fugiat enim ut cillum. Labore enim duis nostrud
+                    eu. Est ut eiusmod consequat irure quis deserunt ex. Enim
+                    laboris dolor magna pariatur. Dolor et ad sint voluptate
+                    sunt elit mollit officia ad enim sit consectetur enim.
+                </p>
+                <div>
+                    <img src="..." />
+                    Lindsay Walton
+                </div>
+            </article>
             <div className="flex ...">
                 <div className="w-1/4 ...">w-1/4</div>
                 <div className="w-3/4 ...">w-3/4</div>
