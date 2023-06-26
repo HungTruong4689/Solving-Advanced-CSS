7f9dc5c93520d55d99764014d714151cfbf080e6
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-26 18:34:12 +0200

    text-right

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -762,6 +762,9 @@ export default function Home() {
             <p className="text-center ...">
                 So I started to walk into the water...
             </p>
+            <p className="text-right ...">
+                So I started to walk into the water...
+            </p>
             <div>
                 <p>
                     I’m Derek, an astro-engineer based in Tattooine. I like to
