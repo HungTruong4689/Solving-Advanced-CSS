9eb0bfb695f73a2a6a0f99a491aa68a31ee7165b
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-28 17:22:49 +0200

    bg-right-top

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -784,6 +784,8 @@ export default function Home() {
             <div className="bg-origin-padding p-4 border-4 border-dashed ..."></div>
             <div className="bg-origin-border p-4 border-4 border-dashed ..."></div>
             <div className="bg-no-repeat bg-top ..."></div>
+            <div className="bg-no-repeat bg-right-top ..."></div>
+
             <div>
                 <p>
                     I’m Derek, an astro-engineer based in Tattooine. I like to
