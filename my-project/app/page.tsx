36c0eabe7ae91e0e9fdafabf984dc101f39cbfd7
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-28 17:23:53 +0200

    bg-right

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -787,6 +787,7 @@ export default function Home() {
             <div className="bg-no-repeat bg-right-top ..."></div>
             <div className="bg-no-repeat bg-left ..."></div>
             <div className="bg-no-repeat bg-center ..."></div>
+            <div className="bg-no-repeat bg-right ..."></div>
 
             <div>
                 <p>
