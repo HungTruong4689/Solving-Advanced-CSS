153542199b2e4f7b0a5febfe4919a0ce638517f4
HungTruong4689 <hungphuc4689@gmail.com>
2023-06-28 17:23:16 +0200

    bg-left

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -785,6 +785,7 @@ export default function Home() {
             <div className="bg-origin-border p-4 border-4 border-dashed ..."></div>
             <div className="bg-no-repeat bg-top ..."></div>
             <div className="bg-no-repeat bg-right-top ..."></div>
+            <div className="bg-no-repeat bg-left ..."></div>
 
             <div>
                 <p>
