a8e66f9d55c542748a1a8ed5d1e51cd26893aa1e
HungTruong4689 <hungphuc4689@gmail.com>
2023-07-04 20:49:36 +0300

    object-contain

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -86,6 +86,9 @@ export default function Home() {
             <div className="bg-indigo-300 ...">
                 <img className="object-cover h-48 w-96 ..." />
             </div>
+            <div className="bg-purple-300 ...">
+                <img className="object-contain h-48 w-96 ..." />
+            </div>
             <div className="p-4">
                 <div className="flow-root ...">
                     <div className="my-4 ...">
