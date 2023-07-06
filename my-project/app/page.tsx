8793806e975a3d1d893b0d4772049d0b092c2795
HungTruong4689 <hungphuc4689@gmail.com>
2023-07-06 19:11:20 +0300

    line-clamp-3

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -809,6 +809,24 @@ export default function Home() {
                 <div>02</div>
                 <div>03</div>
             </div>
+            <article>
+                <time>Mar 10, 2020</time>
+                <h2>Boost your conversion rate</h2>
+                <p className="line-clamp-3">
+                    Nulla dolor velit adipisicing duis excepteur esse in duis
+                    nostrud occaecat mollit incididunt deserunt sunt. Ut ut sunt
+                    laborum ex occaecat eu tempor labore enim adipisicing minim
+                    ad. Est in quis eu dolore occaecat excepteur fugiat dolore
+                    nisi aliqua fugiat enim ut cillum. Labore enim duis nostrud
+                    eu. Est ut eiusmod consequat irure quis deserunt ex. Enim
+                    laboris dolor magna pariatur. Dolor et ad sint voluptate
+                    sunt elit mollit officia ad enim sit consectetur enim.
+                </p>
+                <div>
+                    <img src="..." />
+                    Lindsay Walton
+                </div>
+            </article>
             <button className="outline outline-offset-2 outline-1 ...">
                 Button A
             </button>
