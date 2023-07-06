eafaaeeeae1ec8b3b4d05789845a6b840fce9068
HungTruong4689 <hungphuc4689@gmail.com>
2023-07-06 19:13:35 +0300

    leading-relaxed

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -830,6 +830,9 @@ export default function Home() {
             <p className="leading-normal ...">
                 So I started to walk into the water...
             </p>
+            <p className="leading-relaxed ...">
+                So I started to walk into the water...
+            </p>
             <button className="outline outline-offset-2 outline-1 ...">
                 Button A
             </button>
