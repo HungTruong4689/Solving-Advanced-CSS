06b73d1e0eccb2513110088c8bb4be0971d9a60f
HungTruong4689 <hungphuc4689@gmail.com>
2023-07-06 19:26:44 +0300

    decoration-1

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -836,6 +836,7 @@ export default function Home() {
             <p className="leading-loose ...">
                 So I started to walk into the water...
             </p>
+            <p className="underline decoration-1 ...">The quick brown fox...</p>
             <p className="leading-6 ...">
                 So I started to walk into the water...
             </p>
