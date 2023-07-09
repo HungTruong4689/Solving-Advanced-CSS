05b446f5cd485d1700a6efa43a7999a2f87bd1cd
HungTruong4689 <hungphuc4689@gmail.com>
2023-07-09 21:30:21 +0300

    rounded-none

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -875,6 +875,7 @@ export default function Home() {
             <div className="bg-local ..."></div>
             <button className="rounded-full ...">Save Changes</button>
             <button className="rounded-full ...">Save Changes</button>
+            <button className="rounded-none ...">Save Changes</button>
             <div className="bg-clip-border p-6 bg-violet-600 border-4 border-violet-300 border-dashed"></div>
             <div className="bg-clip-padding p-6 bg-violet-600 border-4 border-violet-300 border-dashed"></div>
             <div className="bg-clip-content p-6 bg-violet-600 border-4 border-violet-300 border-dashed"></div>
