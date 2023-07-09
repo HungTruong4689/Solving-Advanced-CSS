7ad16328ec22b6f54d1129e9b857e5747757b8dc
HungTruong4689 <hungphuc4689@gmail.com>
2023-07-09 21:32:28 +0300

    rounded-r-lg

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -877,6 +877,7 @@ export default function Home() {
             <button className="rounded-full ...">Save Changes</button>
             <button className="rounded-none ...">Save Changes</button>
             <div className="rounded-t-lg ..."></div>
+            <div className="rounded-r-lg ..."></div>
 
             <div className="bg-clip-border p-6 bg-violet-600 border-4 border-violet-300 border-dashed"></div>
             <div className="bg-clip-padding p-6 bg-violet-600 border-4 border-violet-300 border-dashed"></div>
