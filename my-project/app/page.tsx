9240a4495a26604485c8f91eb4cc16dd6530e55f
HungTruong4689 <hungphuc4689@gmail.com>
2023-07-13 20:54:45 +0300

    break-before-avoid

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -885,7 +885,7 @@ export default function Home() {
             </div>
             <div className="columns-2">
                 <p>Well, let me tell you something, ...</p>
-                <p className="break-before-column">Sure, go ahead, laugh...</p>
+                <p className="break-before-avoid">Sure, go ahead, laugh...</p>
                 <p>Maybe we can live without...</p>
                 <p>Look. If you think this is...</p>
             </div>
