017971a14858c21bc44b7a481812304b6380e62e
HungTruong4689 <hungphuc4689@gmail.com>
2023-07-13 20:49:39 +0300

    break-after-avoid

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -879,7 +879,7 @@ export default function Home() {
             <div className="rounded-bl-lg ..."></div>
             <div className="columns-2">
                 <p>Well, let me tell you something, ...</p>
-                <p className="break-after-column">Sure, go ahead, laugh...</p>
+                <p className="break-after-avoid">Sure, go ahead, laugh...</p>
                 <p>Maybe we can live without...</p>
                 <p>Look. If you think this is...</p>
             </div>
