d8932fd9046ed2b8f1fb029835ac82c52184859a
HungTruong4689 <hungphuc4689@gmail.com>
2023-07-13 20:57:33 +0300

    break-before-avoid-page

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -885,7 +885,9 @@ export default function Home() {
             </div>
             <div className="columns-2">
                 <p>Well, let me tell you something, ...</p>
-                <p className="break-before-all">Sure, go ahead, laugh...</p>
+                <p className="break-before-avoid-page">
+                    Sure, go ahead, laugh...
+                </p>
                 <p>Maybe we can live without...</p>
                 <p>Look. If you think this is...</p>
             </div>
