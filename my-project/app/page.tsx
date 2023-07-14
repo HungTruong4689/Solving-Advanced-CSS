928aadbe54aee0762c64ce20db04801bce622007
HungTruong4689 <hungphuc4689@gmail.com>
2023-07-14 20:41:20 +0300

    break-before-page

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -891,6 +891,12 @@ export default function Home() {
                 <p>Maybe we can live without...</p>
                 <p>Look. If you think this is...</p>
             </div>
+            <div className="columns-2">
+                <p>Well, let me tell you something, ...</p>
+                <p className="break-before-page">Sure, go ahead, laugh...</p>
+                <p>Maybe we can live without...</p>
+                <p>Look. If you think this is...</p>
+            </div>
             <div className="ltr">
                 <div className="rounded-s-lg ..."></div>
             </div>
