31d447a87c70965a801654936be1fb9d5dbeba0c
HungTruong4689 <hungphuc4689@gmail.com>
2023-07-14 20:45:50 +0300

    break-inside-avoid-column

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -897,6 +897,14 @@ export default function Home() {
                 <p>Maybe we can live without...</p>
                 <p>Look. If you think this is...</p>
             </div>
+            <div className="columns-2">
+                <p>Well, let me tell you something, ...</p>
+                <p className="break-inside-avoid-column">
+                    Sure, go ahead, laugh...
+                </p>
+                <p>Maybe we can live without...</p>
+                <p>Look. If you think this is...</p>
+            </div>
             <div className="ltr">
                 <div className="rounded-s-lg ..."></div>
             </div>
