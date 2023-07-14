9a777a7e79137495ee21049becbef9cdc5354e40
HungTruong4689 <hungphuc4689@gmail.com>
2023-07-14 20:54:26 +0300

    box-border

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -922,6 +922,9 @@ export default function Home() {
                 <br />
                 World
             </span>
+            <div className="box-border h-32 w-32 p-4 border-4 ...">
+                BOX BORDER
+            </div>
             <span className="box-decoration-clone bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2 ...">
                 Hello
                 <br />
