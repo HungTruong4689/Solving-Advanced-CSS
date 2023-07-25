62251aa7e04a47d3bdfd9846b04d48ea94ab9ae3
HungTruong4689 <hungphuc4689@gmail.com>
2023-07-25 17:14:46 +0300

    font-semibold

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -744,6 +744,7 @@ export default function Home() {
             <p className="font-light ...">The quick brown fox ...</p>
             <p className="font-normal ...">The quick brown fox ...</p>
             <p className="font-medium ...">The quick brown fox ...</p>
+            <p className="font-semibold ...">The quick brown fox ...</p>
 
             <p className="text-sm ...">The quick brown fox ...</p>
             <p className="text-base ...">The quick brown fox ...</p>
