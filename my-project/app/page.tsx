6dc289f6c07378691e37796eeb1c6005cec52fc2
HungTruong4689 <hungphuc4689@gmail.com>
2023-07-27 18:35:47 +0300

    basis-3

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -757,6 +757,7 @@ export default function Home() {
                 <div className="basis-1/2">03</div>
                 <div className="basis-1">04</div>
                 <div className="basis-2">05</div>
+                <div className="basis-3">06</div>
             </div>
             <p className="text-base/6 ...">
                 So I started to walk into the water...
