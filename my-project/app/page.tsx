5860fccf3bee3a0f3de0c8e5724c6d583366e63f
HungTruong4689 <hungphuc4689@gmail.com>
2023-07-28 10:36:19 +0300

    f;ex-initial

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -803,6 +803,8 @@ export default function Home() {
             </div>
             <div className="flex">
                 <div className="flex-none w-14 h-14">01</div>
+                <div className="flex-initial w-64 ...">02</div>
+                <div className="flex-initial w-32 ...">03</div>
             </div>
             <p className="text-base/6 ...">
                 So I started to walk into the water...
