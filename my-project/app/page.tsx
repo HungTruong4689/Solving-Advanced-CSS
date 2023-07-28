41b914521c526dbb524e66a3b5c4410b1395380b
HungTruong4689 <hungphuc4689@gmail.com>
2023-07-28 10:31:08 +0300

    flex-col-reverse

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -776,6 +776,16 @@ export default function Home() {
                 <div>02</div>
                 <div>03</div>
             </div>
+            <div className="flex flex-col ...">
+                <div>01</div>
+                <div>02</div>
+                <div>03</div>
+            </div>
+            <div className="flex flex-col-reverse ...">
+                <div>01</div>
+                <div>02</div>
+                <div>03</div>
+            </div>
             <p className="text-base/6 ...">
                 So I started to walk into the water...
             </p>
