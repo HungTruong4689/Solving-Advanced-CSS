faec5198754a38e5148dc06b0132a46ac33c9294
HungTruong4689 <hungphuc4689@gmail.com>
2023-07-28 10:41:24 +0300

    grow

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -801,6 +801,11 @@ export default function Home() {
                 <div>02</div>
                 <div>03</div>
             </div>
+            <div className="flex ...">
+                <div className="flex-none w-14 h-14 ...">01</div>
+                <div className="grow h-14 ...">02</div>
+                <div className="flex-none w-14 h-14 ...">03</div>
+            </div>
             <div className="flex">
                 <div className="flex-none w-14 h-14">01</div>
                 <div className="flex-initial w-64 ...">02</div>
