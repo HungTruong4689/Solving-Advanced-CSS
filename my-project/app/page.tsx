c5b424cbb29df5ed39dc486b3494054d1115e381
HungTruong4689 <hungphuc4689@gmail.com>
2023-07-29 21:26:10 +0300

    box border

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -857,6 +857,9 @@ export default function Home() {
                 <p>Maybe we can live without...</p>
                 <p>Look. If you think this is...</p>
             </div>
+            <div className="box-border h-32 w-32 p-4 border-4 ...">
+                BOX BORDER
+            </div>
             <div className="flex ...">
                 <div className="flex-none w-14 h-14 ...">01</div>
                 <div className="grow h-14 ...">02</div>
