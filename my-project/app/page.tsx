bcbcfd5a6aff0101e10e6a8103e78b1552b778ab
HungTruong4689 <hungphuc4689@gmail.com>
2023-08-01 21:19:38 +0300

    isolate

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -864,6 +864,7 @@ export default function Home() {
                 BOX CONTENT
             </div>
             <div className="isolate ...">isolate</div>
+            <div className="isolate ...">isolate</div>
             <div className="flex ...">
                 <div className="flex-none w-14 h-14 ...">01</div>
                 <div className="grow h-14 ...">02</div>
