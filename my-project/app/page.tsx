735f9c7c94325d97963534c752526be29c202924
HungTruong4689 <hungphuc4689@gmail.com>
2023-08-01 21:22:11 +0300

    flex-initial

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -880,6 +880,11 @@ export default function Home() {
                 <div className="flex-initial w-64 ...">02</div>
                 <div className="flex-initial w-32 ...">03</div>
             </div>
+            <div className="flex">
+                <div className="flex-none w-14 h-14">01</div>
+                <div className="flex-initial w-64 ...">02</div>
+                <div className="flex-initial w-32 ...">03</div>
+            </div>
             <div className="flex ...">
                 <div className="grow h-14 ...">01</div>
                 <div className="grow-0 h-14 ...">02</div>
