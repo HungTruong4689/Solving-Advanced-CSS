7065855d43be8ffc961aeacb4891b6bb2474cbc3
HungTruong4689 <hungphuc4689@gmail.com>
2023-08-01 21:21:20 +0300

    flex-none

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -870,6 +870,11 @@ export default function Home() {
                 <div className="grow h-14 ...">02</div>
                 <div className="flex-none w-14 h-14 ...">03</div>
             </div>
+            <div className="flex ...">
+                <div className="flex-none w-14 h-14 ...">01</div>
+                <div className="grow h-14 ...">02</div>
+                <div className="flex-none w-14 h-14 ...">03</div>
+            </div>
             <div className="flex">
                 <div className="flex-none w-14 h-14">01</div>
                 <div className="flex-initial w-64 ...">02</div>
