61e5241fb0c341e877451808c7331121f68a0100
HungTruong4689 <hungphuc4689@gmail.com>
2023-08-03 13:00:12 +0300

    flex-row-reverse

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -520,6 +520,11 @@ export default function Home() {
                 <div>02</div>
                 <div>03</div>
             </div>
+            <div className="flex flex-row-reverse ...">
+                <div>01</div>
+                <div>02</div>
+                <div>03</div>
+            </div>
             <div className="grid grid-cols-2 gap-4 place-content-between h-48 ...">
                 <div>01</div>
                 <div>02</div>
