2602eb4e6f3d407b2082d930c91709e202c73201
HungTruong4689 <hungphuc4689@gmail.com>
2023-08-04 23:24:49 +0300

    font-medium

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -636,6 +636,7 @@ export default function Home() {
             <p className="font-light ...">The quick brown fox ...</p>
             <p className="font-light ...">The quick brown fox ...</p>
             <p className="font-normal ...">The quick brown fox ...</p>
+            <p className="font-normal ...">The quick brown fox ...</p>
             <p className="font-medium ...">The quick brown fox ...</p>
             <p className="font-semibold ...">The quick brown fox ...</p>
             <p className="font-bold ...">The quick brown fox ...</p>
