9b54151d418d119bd0077d39c26fcfa9632a745d
HungTruong4689 <hungphuc4689@gmail.com>
2023-08-04 23:22:03 +0300

    text-2xl

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -620,6 +620,7 @@ export default function Home() {
             <p className="text-base ...">The quick brown fox ...</p>
             <p className="text-lg ...">The quick brown fox ...</p>
             <p className="text-xl ...">The quick brown fox ...</p>
+            <p className="text-2xl ...">The quick brown fox ...</p>
             <p className="text-base/7 ...">
                 So I started to walk into the water...
             </p>
