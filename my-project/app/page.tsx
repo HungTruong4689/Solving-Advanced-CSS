da8d72a0df08fb70e942625ccc75d29f37d8f1e1
HungTruong4689 <hungphuc4689@gmail.com>
2023-08-06 20:14:21 +0300

    static

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -231,6 +231,9 @@ export default function Home() {
                 <div className="static ...">
                     <p>Static child</p>
                 </div>
+                <div className="static ...">
+                    <p>Static child</p>
+                </div>
                 <div className="inline-block ...">
                     <p>Static sibling</p>
                 </div>
