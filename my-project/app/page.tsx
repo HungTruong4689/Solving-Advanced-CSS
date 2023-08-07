3c4d7c64792a0d54e6e1b2c0f2ddacf367a86847
HungTruong4689 <hungphuc4689@gmail.com>
2023-08-07 22:27:43 +0300

    text-left

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -637,6 +637,9 @@ export default function Home() {
             <p className="text-left ...">
                 So I started to walk into the water...
             </p>
+            <p className="text-left ...">
+                So I started to walk into the water...
+            </p>
             <p className="text-center ...">
                 So I started to walk into the water...
             </p>
