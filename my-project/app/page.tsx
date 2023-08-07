3cc3bcb21da4f2924f96b49a163eb310364cf7d2
HungTruong4689 <hungphuc4689@gmail.com>
2023-08-07 22:34:16 +0300

    inline-block

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -273,6 +273,9 @@ export default function Home() {
                 <div className="inline-block ...">
                     <p>Static sibling</p>
                 </div>
+                <div className="inline-block ...">
+                    <p>Static sibling</p>
+                </div>
 
                 <div className="absolute ...">
                     <p>Absolute child</p>
