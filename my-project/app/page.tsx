d80c0dfaa53f4cb96a880a1a17440db5444dfd3e
HungTruong4689 <hungphuc4689@gmail.com>
2023-08-11 22:54:54 +0300

    content-around

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -403,7 +403,7 @@ export default function Home() {
                 <div>05</div>
             </div>
             <div className="h-56 grid grid-cols-3 gap-4 content-around ...">
-                <div>01</div>
+                <div>01 align-content: space-around;</div>
                 <div>02</div>
                 <div>03</div>
                 <div>04</div>
