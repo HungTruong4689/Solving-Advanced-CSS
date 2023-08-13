ab42f331b582dc5ba49a36a19f30647a3e6cf8b8
HungTruong4689 <hungphuc4689@gmail.com>
2023-08-13 19:24:01 +0300

    grow exp

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -39,7 +39,10 @@ export default function Home() {
             </div>
             <div className="flex ...">
                 <div className="flex-none w-14 h-14 ...">01</div>
-                <div className="grow h-14 ...">02</div>
+                <div className="grow h-14 ...">
+                    02 Use grow to allow a flex item to grow to fill any
+                    available space:
+                </div>
                 <div className="flex-none w-14 h-14 ...">03</div>
             </div>
             <span className="inline-grid grid-cols-3 gap-4">
