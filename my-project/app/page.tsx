004062b1ae3bb6541ead3c6492da6b3b0ea7740e
HungTruong4689 <hungphuc4689@gmail.com>
2023-08-13 19:24:49 +0300

    grow-0

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -45,6 +45,11 @@ export default function Home() {
                 </div>
                 <div className="flex-none w-14 h-14 ...">03</div>
             </div>
+            <div className="flex ...">
+                <div className="grow h-14 ...">01</div>
+                <div className="grow-0 h-14 ...">02</div>
+                <div className="grow h-14 ...">03</div>
+            </div>
             <span className="inline-grid grid-cols-3 gap-4">
                 <span>01</span>
                 <span>02</span>
