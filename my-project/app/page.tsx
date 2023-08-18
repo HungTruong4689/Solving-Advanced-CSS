00ce20aedb3d68042f8c3e0cf567513468a01180
HungTruong4689 <hungphuc4689@gmail.com>
2023-08-18 21:41:02 +0300

    fixed

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -259,6 +259,19 @@ export default function Home() {
                     <p>Static sibling</p>
                 </div>
             </div>
+            <div className="relative">
+                <div className="fixed top-0 left-0 right-0">Contacts</div>
+                <div>
+                    <div>
+                        <img src="..." />
+                        <strong>Andrew Alfred</strong>
+                    </div>
+                    <div>
+                        <img src="..." />
+                        <strong>Debra Houston</strong>
+                    </div>
+                </div>
+            </div>
             <img className="float-left ..." src="path/to/image.jpg" />
             <img className="float-right ..." src="path/to/image.jpg" />
             <img
