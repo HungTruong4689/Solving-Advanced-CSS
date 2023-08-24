84edffa138e967ba269f7c68dd081b67bcb97d43
HungTruong4689 <hungphuc4689@gmail.com>
2023-08-24 17:16:14 +0300

    outline -4

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -368,6 +368,9 @@ export default function Home() {
             <button className="outline outline-offset-2 outline-2 ...">
                 Button B
             </button>
+            <button className="outline outline-offset-2 outline-4 ...">
+                Button C
+            </button>
 
             <button className="border-hidden ...">
                 Save Changes border-style: hidden;
