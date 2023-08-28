006017d62f1c60fbc797afd210b093ab31d142f3
HungTruong4689 <hungphuc4689@gmail.com>
2023-08-28 21:37:10 +0300

    absolute

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -642,6 +642,9 @@ export default function Home() {
                 <div className="absolute ...">
                     <p>Absolute child</p>
                 </div>
+                <div className="absolute ...">
+                    <p>Absolute child</p>
+                </div>
                 <div className="inline-block ...">
                     <p>Static sibling</p>
                 </div>
