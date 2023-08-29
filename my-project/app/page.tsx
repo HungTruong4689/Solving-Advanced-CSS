164c5fc2c78e2e38b55894d22c288062a58fe516
HungTruong4689 <hungphuc4689@gmail.com>
2023-08-29 21:15:56 +0300

    basis-size

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -40,7 +40,10 @@ export default function Home() {
                 <div className="flex-1 ...">03</div>
             </div>
             <div className="flex flex-row">
-                <div className="basis-1/4">01</div>
+                <div className="basis-1/4">
+                    01 Use the basis-{size} utilities to set the initial size of
+                    flex items.
+                </div>
                 <div className="basis-1/4">02</div>
                 <div className="basis-1/2">03</div>
             </div>
