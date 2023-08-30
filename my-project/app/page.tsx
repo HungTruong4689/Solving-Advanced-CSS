266447c25b2ce914ee975ae58f673659a9acc555
HungTruong4689 <hungphuc4689@gmail.com>
2023-08-30 20:50:02 +0300

    box-decoration-slice

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -95,6 +95,11 @@ export default function Home() {
                     </div>
                 </div>
             </div>
+            <span className="box-decoration-slice bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2 ...">
+                Hello
+                <br />
+                World
+            </span>
             <div className="flex ...">
                 <div className="flex-none w-14 h-14 ...">01</div>
                 <div className="grow h-14 ...">
