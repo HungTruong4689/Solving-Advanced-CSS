0af6d6b07719a32461a6bdfd1718ee9b6165338e
HungTruong4689 <hungphuc4689@gmail.com>
2023-09-04 22:26:36 +0300

    overscroll-contain

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -759,6 +759,9 @@ export default function Home() {
             <div className="overscroll-contain ...">
                 Well, let me tell you something, ...
             </div>
+            <div className="overscroll-contain ...">
+                Well, let me tell you something, ...
+            </div>
             <div className="overscroll-auto ...">
                 Well, let me tell you something, ...
             </div>
