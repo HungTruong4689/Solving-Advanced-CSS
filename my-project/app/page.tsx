f5adfb3512875e5d3f2a142995e42c0cb7e0fd4b
HungTruong4689 <hungphuc4689@gmail.com>
2023-09-05 21:35:54 +0300

    object contain

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -802,6 +802,9 @@ export default function Home() {
                 Resize an element’s content to cover its container using
                 object-cover.
             </div>
+            <div className="bg-purple-300 ...">
+                <img className="object-contain h-48 w-96 ..." />
+            </div>
             <div className="bg-indigo-300 ...">
                 <img className="object-cover h-48 w-96 ..." />
             </div>
