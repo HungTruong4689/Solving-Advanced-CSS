3386c11386be2d65fbeca1690f08f5dccf4a6293
HungTruong4689 <hungphuc4689@gmail.com>
2023-09-06 21:00:11 +0300

    flex row

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -886,6 +886,11 @@ export default function Home() {
                 <div>02</div>
                 <div>03</div>
             </div>
+            <div className="flex flex-row ...">
+                <div>01</div>
+                <div>02</div>
+                <div>03</div>
+            </div>
             <div className="flex flex-col ...">
                 <div>01</div>
                 <div>02</div>
