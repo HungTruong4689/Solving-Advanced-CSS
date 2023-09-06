891832d87987179ff06ee398e416434774748579
HungTruong4689 <hungphuc4689@gmail.com>
2023-09-06 21:03:32 +0300

    flex wrap reverse

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -927,6 +927,11 @@ export default function Home() {
                 <div>02</div>
                 <div>03</div>
             </div>
+            <div className="flex flex-wrap-reverse">
+                <div>01</div>
+                <div>02</div>
+                <div>03</div>
+            </div>
             <div className="flex flex-nowrap">
                 <div>01</div>
                 <div>02</div>
