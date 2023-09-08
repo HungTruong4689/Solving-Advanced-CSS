e71c863f525d2e82157bbd95d5ca19f50d30b595
HungTruong4689 <hungphuc4689@gmail.com>
2023-09-08 12:51:54 +0300

    col end

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -1163,6 +1163,8 @@ export default function Home() {
             <div className="grid grid-cols-6 gap-4">
                 <div className="col-start-2 col-span-4 ...">01</div>
                 <div className="col-start-1 col-end-3 ...">02</div>
+                <div className="col-end-7 col-span-2 ...">03</div>
+                <div className="col-start-1 col-end-7 ...">04</div>
             </div>
             <div className="h-56 grid grid-cols-3 gap-4 content-normal ...">
                 <div>01</div>
