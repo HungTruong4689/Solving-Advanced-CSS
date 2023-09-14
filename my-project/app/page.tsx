73531a822f953f0aff666629ff37c83586e46909
HungTruong4689 <hungphuc4689@gmail.com>
2023-09-14 21:31:21 +0300

    text-xl

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -1903,6 +1903,7 @@ export default function Home() {
             <p className="text-lg ...">The quick brown fox ...</p>
             <p className="text-lg ...">The quick brown fox ...</p>
             <p className="text-xl ...">The quick brown fox ...</p>
+            <p className="text-xl ...">The quick brown fox ...</p>
             <p className="text-2xl ...">The quick brown fox ...</p>
             <div className="flex flex-row">
                 <div className="basis-1/4">01</div>
