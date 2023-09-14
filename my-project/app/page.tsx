4e056761289fa65c70d60a8c5d3abd18f3548e0f
HungTruong4689 <hungphuc4689@gmail.com>
2023-09-14 21:29:32 +0300

    text-base

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -1899,6 +1899,7 @@ export default function Home() {
             <p className="text-sm ...">The quick brown fox ...</p>
             <p className="text-sm ...">The quick brown fox ...</p>
             <p className="text-base ...">The quick brown fox ...</p>
+            <p className="text-base ...">The quick brown fox ...</p>
             <p className="text-lg ...">The quick brown fox ...</p>
             <p className="text-xl ...">The quick brown fox ...</p>
             <p className="text-2xl ...">The quick brown fox ...</p>
