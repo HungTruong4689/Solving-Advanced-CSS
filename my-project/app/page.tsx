42847542416885396c8dc89a977207baddcfa1cb
HungTruong4689 <hungphuc4689@gmail.com>
2023-09-15 22:48:40 +0300

    break-after avoid

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -1984,6 +1984,7 @@ export default function Home() {
             <div className="columns-2">
                 <p>Well, let me tell you something, ...</p>
                 <p className="break-after-avoid">Sure, go ahead, laugh...</p>
+                <p className="break-after-avoid">Sure, go ahead, laugh...</p>
                 <p>Maybe we can live without...</p>
                 <p>Look. If you think this is...</p>
             </div>
