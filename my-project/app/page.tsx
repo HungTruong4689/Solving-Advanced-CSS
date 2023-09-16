999cfbbfb9c93062a070457515602aea5112d2fb
HungTruong4689 <hungphuc4689@gmail.com>
2023-09-16 23:15:44 +0300

    divide-y

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -2143,7 +2143,7 @@ export default function Home() {
                 <div>03</div>
             </div>
             <div className="grid grid-cols-1 divide-y">
-                <div>01</div>
+                <div className="divide-y">01</div>
                 <div>02</div>
                 <div>03</div>
             </div>
