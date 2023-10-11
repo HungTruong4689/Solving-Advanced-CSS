ad36ef502af197792ff52417a8eda21b5c859738
HungTruong4689 <hungphuc4689@gmail.com>
2023-10-11 17:06:01 +0200

    leading relaxed

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -2704,6 +2704,9 @@ export default function Home() {
             <p className="leading-relaxed ...">
                 So I started to walk into the water...
             </p>
+            <p className="leading-relaxed ...">
+                So I started to walk into the water...
+            </p>
             <p className="leading-loose ...">
                 So I started to walk into the water...
             </p>
