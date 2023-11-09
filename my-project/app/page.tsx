c96e60a908cdef98f662e425b950c52e00d0cbec
HungTruong4689 <hungphuc4689@gmail.com>
2023-11-09 20:08:42 +0100

    bg-fixed

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -2259,6 +2259,7 @@ export default function Home() {
             <div className="rounded-lg ...">rounded-lg</div>
             <div className="rounded-full ...">rounded-full</div>
             <div className="bg-fixed ...">bg-fixed</div>
+            <div className="bg-fixed ...">bg-fixed</div>
             <div className="bg-local ...">bg-local</div>
             <div className="rounded-tl-lg ...">rounded-tl-lg</div>
             <div className="rounded-tr-lg ...">rounded-tr-lg</div>
