2b8489e6974fcbaca1f4b8a31ef4e7819593f852
HungTruong4689 <hungphuc4689@gmail.com>
2023-11-14 22:02:38 +0100

    break before colum2

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -2293,6 +2293,12 @@ export default function Home() {
             </div>
             <div className="columns-2">
                 <p>Well, let me tell you something, ...</p>
+                <p className="break-before-column">Sure, go ahead, laugh...</p>
+                <p>Maybe we can live without...</p>
+                <p>Look. If you think this is...</p>
+            </div>
+            <div className="columns-2">
+                <p>Well, let me tell you something, ...</p>
                 <p className="break-inside-avoid-column">
                     Sure, go ahead, laugh...
                 </p>
