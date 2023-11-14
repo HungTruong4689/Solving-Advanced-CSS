fd26cf1643545c49e50fd58aa37163be4891c9b9
HungTruong4689 <hungphuc4689@gmail.com>
2023-11-14 22:03:36 +0100

    break inside auto check

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -2313,6 +2313,12 @@ export default function Home() {
             </div>
             <div className="columns-2">
                 <p>Well, let me tell you something, ...</p>
+                <p className="break-inside-auto">Sure, go ahead, laugh...</p>
+                <p>Maybe we can live without...</p>
+                <p>Look. If you think this is...</p>
+            </div>
+            <div className="columns-2">
+                <p>Well, let me tell you something, ...</p>
                 <p className="break-inside-avoid">Sure, go ahead, laugh...</p>
                 <p>Maybe we can live without...</p>
                 <p>Look. If you think this is...</p>
